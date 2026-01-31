78be73e57b01bf0c33a5d4e4750bd154c300e8f7
M-RakeshSushanth <maremallarakesh07@gmail.com>
2026-01-31 21:59:19 +0530

    latest change

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -13,16 +13,17 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv(BASE_DIR / '.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = os.getenv("DEBUG") == "True"
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
